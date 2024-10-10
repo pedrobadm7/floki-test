bf018fcd3838caf9d21a4038edeaf046e62d1f11
Pedro Barros <pedrobars7a@gmail.com>
2024-10-10 18:24:44 -0300

    feat: add rules to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import importPlugin from 'eslint-plugin-import'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -16,13 +17,36 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'import': importPlugin
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'import/order': [
+        'error',
+        {
+          'groups': [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type'
+          ],
+          'alphabetize': {
+            'order': 'asc',
+            'caseInsensitive': true
+          },
+          'newlines-between': 'always'
+        }
+      ]
     },
   },
 )
