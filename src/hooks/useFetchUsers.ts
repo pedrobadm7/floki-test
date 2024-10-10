7fd4db155bbd402eb185ffcb39ce36c28a0b5756
Pedro Barros <pedrobars7a@gmail.com>
2024-10-10 16:38:08 -0300

    feat: react query initial setup

---
+++ src/hooks/useFetchUsers.ts
@@ -0,0 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const fetchUsers = async () => {
+  const { data } = await axios.get('https://randomuser.me/api/?results=10')
+  return data.results
+}
+
+export const useFetchUsers = () => {
+  const { data } = useQuery({ queryKey: ['users'], queryFn: fetchUsers });
+
+  return data;
+}
